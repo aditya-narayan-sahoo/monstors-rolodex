8d60489307300a4b7429eeab889c008d77eac517
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-07-21 11:18:48 +0530

    Initial files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        { name: "Franklestein", id: "asc1" },
+        { name: "Dracula", id: "asc2" },
+        { name: "Zombie", id: "asc3" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
